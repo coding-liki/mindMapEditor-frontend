2da10422c8b7f08feacec2dc88455c1c5efe5d94
lnekrasov <lnekrasov@beget.ru>
2022-02-08 18:52:32 +0300

    update generator

--- src/Svg/RandomGenerator.js
+++ src/Svg/RandomGenerator.js
@@ -1,7 +1,10 @@
 export default class RandomGenerator {
-    generate(width, height, minAmplitude, maxAmplitude, verges){
+    generateBorder(width, height, minAmplitude, maxAmplitude, verges){
         let path = document.createElementNS('http://www.w3.org/2000/svg',"path");  
         let svg = document.createElementNS('http://www.w3.org/2000/svg',"svg");
+        
+        let a = width/2;
+        let b = height/2;
         
     }
 };
